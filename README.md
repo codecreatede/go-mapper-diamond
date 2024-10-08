31f96a8557f2db9261dbf6b4e6988128cb06077f
Gaurav Sablok <gauravcodepro@gmail.com>
2024-10-08 14:54:48 +0200

    codecreatede
    finished writing and compiling the code, completed the README and preparing for the final release including the binary release.

--- README.md
+++ README.md
@@ -1,9 +1,99 @@
 # go-mapper-diamond 
 
-- a dimaond mapping of the pacbiohifi reads to the protein databases. 
+- a dimaond mapping of the pacbiohifi reads/fasta to the protein databases. 
 - It just needs the pacbiohifi reads and the protein database and it aligns them and extracts the regions and store them as structs and interfaces.  
 - diamond mapper can be found at [diamond mapper](https://github.com/bbuchfink/diamond)
-- for hint generation and tag generation for AUGUSTUS and BRAKER. 
+- for hint generation and tag generation for AUGUSTUS and BRAKER and the sequence extraction for generating the environmental tags for the sequencing. 
 
+```
+gauavsablok@gauravsablok ~/Desktop/codecreatede/golang/gomapper-diamond ±main⚡ » go run main.go -h
+This is golang application for generating the hints from the protein alignment to pacbiohifi reads
+
+Usage:
+  flag [command]
+
+Available Commands:
+  align
+  completion       Generate the autocompletion script for the specified shell
+  help             Help about any command
+  hspalignment
+  pacbio
+  proteinalignment
+  seqHsp
+  upStreamHSP
+
+Flags:
+  -h, --help   help for flag
+
+Use "flag [command] --help" for more information about a command.
+gauavsablok@gauravsablok ~/Desktop/codecreatede/golang/gomapper-diamond ±main⚡ » \
+go run main.go align -h
+this option aligns and analyze all together and it requires only the proteins and the reference pacbio or the other fasta file
+
+Usage:
+  flag align and analyze [flags]
+
+Flags:
+  -h, --help                  help for align
+  -p, --pacbio string         read-protein alignment (default "reads for protein alignment")
+  -f, --pacbiofolder string   pacbio conversion (default "folder containing the bam files and the pbi files")
+  -P, --protein string        protein datasets for the alignment (default "protein datasets")
+gauavsablok@gauravsablok ~/Desktop/codecreatede/golang/gomapper-diamond ±main⚡ » \
+go run main.go hspalignment -h
+Analyzes the hsp from the diamond read to protein alignment
+
+Usage:
+  flag hspalignment [flags]
+
+Flags:
+  -a, --alignmentfile string    alignment (default "alignment file to be analyzed")
+  -h, --help                    help for hspalignment
+  -p, --referencefasta string   pacbio file (default "pacbio reads file")
+gauavsablok@gauravsablok ~/Desktop/codecreatede/golang/gomapper-diamond ±main⚡ » \
+go run main.go pacbio -h
+converts the pacbio reads to the fasta format for the alignment and annotations, provide the folder path
+
+Usage:
+  flag pacbio [flags]
+
+Flags:
+  -h, --help                  help for pacbio
+  -f, --pacbiofolder string   pacbio conversion (default "folder containing the bam files and the pbi files")
+gauavsablok@gauravsablok ~/Desktop/codecreatede/golang/gomapper-diamond ±main⚡ » \
+go run main.go proteinalignment -h
+aligns the pacbio reads or the fasta reads to the proteins
+
+Usage:
+  flag proteinalignment [flags]
+
+Flags:
+  -h, --help             help for proteinalignment
+  -p, --pacbio string    read-protein alignment (default "reads for protein alignment")
+  -P, --protein string   protein datasets for the alignment (default "protein datasets")
+gauavsablok@gauravsablok ~/Desktop/codecreatede/golang/gomapper-diamond ±main⚡ » \
+go run main.go seqHsp -h                                                
+Analyzes the hsp from the diamond read to protein alignment
+
+Usage:
+  flag seqHsp [flags]
+
+Flags:
+  -a, --alignmentfile string    alignment (default "alignment file to be analyzed")
+  -h, --help                    help for seqHsp
+  -p, --referencefasta string   pacbio file (default "pacbio reads file")
+gauavsablok@gauravsablok ~/Desktop/codecreatede/golang/gomapper-diamond ±main⚡ » \
+go run main.go upStreamHSP -h
+specific for the genome alignment regions upstream and the downstream of the alignments
+
+Usage:
+  flag upStreamHSP [flags]
+
+Flags:
+  -a, --alignmentfile string             alignment (default "alignment file to be analyzed")
+  -d, --downstream of the hsp tags int   downstream tags (default 5)
+  -h, --help                             help for upStreamHSP
+  -p, --referencefasta string            pacbio file (default "pacbio reads file")
+  -u, --upstream of the hsp tags int     upstream tags (default 4)
+```
 
 Gaurav Sablok
--- main.go
+++ main.go
@@ -10,179 +10,541 @@ Date 2024-9-11
 doing the denovo alignment of the pacbiohifi reads against a protein database and holding those
 alignment and the corresponding alignments in struct and extracting the matched regions. This is for
 when you have a relatively less coverage and you want to search for the tags and the corresponding
-protein alignments to generate annotation hints.
+protein alignments to generate annotation hints.  It has the following functions:
+1. func readalignment : This aligns the reads to the protein datasets and prepares them for all the analysis.
+2. func fastalignment : This prepares the fasta files for the alignment and prepares them for the analysis.
+3. func tags: This prepares the coverage estimation for the alignment regions.
+4. func sequence hints: This extracts the sequences upstream and the downstream
 
 */
 
-
 import (
+	"bufio"
 	"fmt"
-	"flag"
+	"log"
 	"os"
 	"os/exec"
-	"log"
+	"strconv"
 	"strings"
+
 	"github.com/spf13/cobra"
 )
 
-func main () {
+func main() {
 	if err := rootCmd.Execute(); err != nil {
 		log.Fatal(err)
 		os.Exit(1)
 	}
+}
 
-var pacbio string
-var protein string
-var alignment string
+var (
+	pacbiofolder   string
+	proteinfasta   string
+	alignmentfile  string
+	referencefasta string
+	upstreamStart  int
+	downstreamEnd  int
+	pacbiofile     string
+)
 
-var rootCmd = &cobra.Command {
-	Use: "flag",
+var rootCmd = &cobra.Command{
+	Use:  "flag",
 	Long: "This is golang application for generating the hints from the protein alignment to pacbiohifi reads",
 }
 
-var alignCmd = &cobra.Command {
-	Use: "align",
-	Long: "aligns the reads to the protein database and then generates the hints",
-	Run: alignFunc,
-
+var pacbioBamCmd = &cobra.Command{
+	Use:  "pacbio",
+	Long: "converts the pacbio reads to the fasta format for the alignment and annotations, provide the folder path",
+	Run:  convertFunc,
 }
 
-var analyzeCmd = &cobra.Command {
-	Use: "analyze",
-	Long: "analyze the already aligned reads to protein alignment",
-	Run: analyzeFunc,
+var alignmentCmd = &cobra.Command{
+	Use:  "proteinalignment",
+	Long: "aligns the pacbio reads or the fasta reads to the proteins",
+	Run:  alignCmd,
 }
 
+var hspCmd = &cobra.Command{
+	Use:  "hspalignment",
+	Long: "Analyzes the hsp from the diamond read to protein alignment",
+	Run:  hspFunc,
+}
+
+var seqCmd = &cobra.Command{
+	Use:  "seqHsp",
+	Long: "Analyzes the hsp from the diamond read to protein alignment",
+	Run:  getSeqFunc,
+}
+
+var upstreamCmd = &cobra.Command{
+	Use:  "upStreamHSP",
+	Long: "specific for the genome alignment regions upstream and the downstream of the alignments",
+	Run:  upstreamFunc,
+}
+
+var analyzeCmd = &cobra.Command{
+	Use:  "align and analyze",
+	Long: "this option aligns and analyze all together and it requires only the proteins and the reference pacbio or the other fasta file",
+	Run:  alignAnalyzeFunc,
+}
 
 func init() {
+	pacbioBamCmd.Flags().
+		StringVarP(&pacbiofolder, "pacbiofolder", "f", "folder containing the bam files and the pbi files", "pacbio conversion")
+	alignmentCmd.Flags().
+		StringVarP(&pacbiofile, "pacbio", "p", "reads for protein alignment", "read-protein alignment")
+	alignmentCmd.Flags().
+		StringVarP(&proteinfasta, "protein", "P", "protein datasets", "protein datasets for the alignment")
+	hspCmd.Flags().
+		StringVarP(&alignmentfile, "alignmentfile", "a", "alignment file to be analyzed", "alignment")
+	hspCmd.Flags().
+		StringVarP(&referencefasta, "referencefasta", "p", "pacbio reads file", "pacbio file")
+	seqCmd.Flags().
+		StringVarP(&alignmentfile, "alignmentfile", "a", "alignment file to be analyzed", "alignment")
+	seqCmd.Flags().
+		StringVarP(&referencefasta, "referencefasta", "p", "pacbio reads file", "pacbio file")
+	upstreamCmd.Flags().
+		StringVarP(&alignmentfile, "alignmentfile", "a", "alignment file to be analyzed", "alignment")
+	upstreamCmd.Flags().
+		StringVarP(&referencefasta, "referencefasta", "p", "pacbio reads file", "pacbio file")
+	upstreamCmd.Flags().
+		IntVarP(&upstreamStart, "upstream of the hsp tags", "u", 4, "upstream tags")
+	upstreamCmd.Flags().
+		IntVarP(&downstreamEnd, "downstream of the hsp tags", "d", 5, "downstream tags")
+	analyzeCmd.Flags().
+		StringVarP(&pacbiofolder, "pacbiofolder", "f", "folder containing the bam files and the pbi files", "pacbio conversion")
+	analyzeCmd.Flags().
+		StringVarP(&pacbiofile, "pacbio", "p", "reads for protein alignment", "read-protein alignment")
+	analyzeCmd.Flags().
+		StringVarP(&proteinfasta, "protein", "P", "protein datasets", "protein datasets for the alignment")
 
-	alignCmd.Flags().StringVarP(&pacbio, "pacbio", "p", "reads for protein alignment", "read-protein alignment")
-	alignCmd.Flags().StringVarP(&protein, "protein", "P", "protein datasets", "protein datasets for the alignment")
-	analyzeCmd.Flags().StringVarP(&alignment, "alignment", "a", "analyze the already given alignment", "post alignment analyzer")
-
-  rootCmd.AddCommand(alignCmd)
-  rootCmd.AddCommand(analyzeCmd)
-
+	rootCmd.AddCommand(seqCmd)
+	rootCmd.AddCommand(upstreamCmd)
+	rootCmd.AddCommand(pacbioBamCmd)
+	rootCmd.AddCommand(alignmentCmd)
+	rootCmd.AddCommand(hspCmd)
+	rootCmd.AddCommand(analyzeCmd)
 }
 
-
-func alignCmd (cmd *cobra.Command, args [] string){
-
-	type mapperID struct {
-		id string
-	}
-	type mapperSeq struct {
-		seq string
-	}
-
-	pacbioOpen, err := os.Open(pacbio)
+func convertFunc(cmd *cobra.Command, args []string) {
+	convert, err := exec.Command("pbtk", "-o", "pacbio.fasta", "*.bam").Output()
 	if err != nil {
-		log.Fatal (err)
+		log.Fatal(err)
+	}
+	fmt.Println(string(convert))
+}
+
+func alignCmd(cmd *cobra.Command, args []string) {
+	proteinreference := proteinfasta
+	makedb, err := exec.Command("diamond", "makedb", "-in", proteinreference, "-d", "reference").
+		Output()
+	if err != nil {
+		log.Fatal("command failed with %s\n", err)
+	}
+	fmt.Println(string(makedb))
+	align, err := exec.Command("diamond", "blastx", "-d", "reference", "-q", "pacbio.fasta", "-o", "pacbioaligned.tsv").
+		Output()
+	if err != nil {
+		log.Fatal("command failed with the error %s\n", err)
+	}
+	fmt.Println(string(align))
+}
+
+func sum(arr []float64) float64 {
+	counter := float64(0)
+	for i := range arr {
+		counter += arr[i]
+	}
+	return counter
+}
+
+func pacbio() ([]string, []string, []float64) {
+	readOpen, err := os.Open(referencefasta)
+	if err != nil {
+		log.Fatal(err)
 	}
 
-	pacbioRead := bufio.NewScanner(pacbioOpen)
+	readbuffer := bufio.NewScanner(readOpen)
+	header := []string{}
+	sequences := []string{}
+	length := []float64{}
 
-  seqID = []mapperID{}
-  seqSeq = []mapperSeq{}
-
-  for pacbioRead.Scan() {
-		line := pacBioRead.Text()
-		if strings.HasPrefix(string(line), "@") {
-			seqID = append(seqID, mapperID{
-        id : string.Split(string(line), "\t")[0],
-			})
-		if strings.HasPrefix(string(line), "A") || strings.HasPrefix(string(line), "T") || strings.HasPrefix(string(line), "C") || strings.HasPrefix(string(line), "C")
-			seqSeq = append(seqSeq, mapperSeq{
-				seq : string(line)
-			})
-			combinedSeqID = []{seqID, seqSeq}
+	for readbuffer.Scan() {
+		line := readbuffer.Text()
+		if string(line[0]) == "A" || string(line[0]) == "T" || string(line[0]) == "G" ||
+			string(line[0]) == "C" {
+			sequences = append(sequences, line)
 		}
-			writeFasta, err := os.Create("pacbio.fasta", os.O_CREATE|os.O_WRONLY, 0644)
-		  if err ! = nil {
-			log.Fatal(err)
-		 }
-		 defer writeFasta.Close()
-     for i := range seqID {
-			for j := range seqSeq {
-				write, err := writeFasta.write(>seqID[i]\nseqSeq[j])
-				if err ! = nil {
-					log.Fatal(err)
-				}
-			}
+		if string(line[0]) == ">" {
+			header = append(header, strings.ReplaceAll(string(line), ">", ""))
+		}
+	}
+	for i := range sequences {
+		length = append(length, float64(len(sequences[i])))
+	}
+	return header, sequences, length
+}
 
-	  out, err := exec.Command("diamond", "makedb", "-in", "pacbio.fasta", "-d", "reference").Output()
+func hspFunc(cmd *cobra.Command, args []string) {
+	refID := []string{}
+	alignID := []string{}
+	refIdenStart := []float64{}
+	refIdenEnd := []float64{}
+	alignIdenStart := []float64{}
+	alignIdenEnd := []float64{}
+	fOpen, err := os.Open(alignmentfile)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fRead := bufio.NewScanner(fOpen)
+
+	for fRead.Scan() {
+		line := fRead.Text()
+		refID = append(refID, strings.Split(string(line), "\t")[0])
+		alignID = append(alignID, strings.Split(string(line), "\t")[1])
+		start1, _ := strconv.ParseFloat(strings.Split(string(line), "\t")[6], 32)
+		end1, _ := strconv.ParseFloat(strings.Split(string(line), "\t")[7], 32)
+		start2, _ := strconv.ParseFloat(strings.Split(string(line), "\t")[8], 32)
+		end2, _ := strconv.ParseFloat(strings.Split(string(line), "\t")[9], 32)
+		refIdenStart = append(refIdenStart, start1)
+		refIdenEnd = append(refIdenEnd, end1)
+		alignIdenStart = append(alignIdenStart, start2)
+		alignIdenEnd = append(alignIdenEnd, end2)
+	}
+	id, _, length := pacbio()
+
+	type cov struct {
+		id  string
+		cov float64
+	}
+
+	coverageSeq := []cov{}
+	for i := range id {
+		for j := range refID {
+			if id[i] == refID[j] {
+				coverageSeq = append(coverageSeq, cov{
+					id:  refID[j],
+					cov: (refIdenEnd[j] - refIdenStart[j]) / length[i] * 100,
+				})
+			}
+		}
+	}
+
+	for i := range coverageSeq {
+		fmt.Println(coverageSeq[i].id, coverageSeq[i].cov)
+	}
+
+	file, err := os.Create("coveragestimation.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+	for i := range coverageSeq {
+		storeI := strconv.FormatFloat(coverageSeq[i].cov, 'f', -1, 64)
+		_, err := file.WriteString(coverageSeq[i].id + "\t" + storeI + "\n")
 		if err != nil {
-					log.Fatal("command failed with %s\n", err)
-				}
-		fmt.Println(out)
+			log.Fatal(err)
+		}
+	}
+}
 
-		out, err := exec.Command("diamond", "blastx", "-d", "reference", "-q", "*readsFasta", "-o", "aligned.tsv").Output()
-	  if err != nil {
-					log.Fatal("command failed with the error %s\n", err)
-				}
-		fmt.Println(out)
-		 }
+func getSeqFunc(cmd *cobra.Command, args []string) {
+	refID := []string{}
+	alignID := []string{}
+	refIdenStart := []float64{}
+	refIdenEnd := []float64{}
+	alignIdenStart := []float64{}
+	alignIdenEnd := []float64{}
+	fOpen, err := os.Open(alignmentfile)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-	type hspStruct struct {
-       refseq string
-			 aligned string
-			 alignmentMatch string
-			 start string
-			 end string
-			 evalue string
+	fRead := bufio.NewScanner(fOpen)
+
+	for fRead.Scan() {
+		line := fRead.Text()
+		refID = append(refID, strings.Split(string(line), "\t")[0])
+		alignID = append(alignID, strings.Split(string(line), "\t")[1])
+		start1, _ := strconv.ParseFloat(strings.Split(string(line), "\t")[6], 32)
+		end1, _ := strconv.ParseFloat(strings.Split(string(line), "\t")[7], 32)
+		start2, _ := strconv.ParseFloat(strings.Split(string(line), "\t")[8], 32)
+		end2, _ := strconv.ParseFloat(strings.Split(string(line), "\t")[9], 32)
+		refIdenStart = append(refIdenStart, start1)
+		refIdenEnd = append(refIdenEnd, end1)
+		alignIdenStart = append(alignIdenStart, start2)
+		alignIdenEnd = append(alignIdenEnd, end2)
+	}
+
+	seqIDType, seqSeqType := readRef()
+
+	type extractSeq struct {
+		extractPartID  string
+		extractPartSeq string
+	}
+
+	extractPartSeq := []extractSeq{}
+
+	for i := range seqIDType {
+		for j := range refID {
+			if seqIDType[i] == refID[j] {
+				extractPartSeq = append(extractPartSeq, extractSeq{
+					extractPartID:  seqIDType[i],
+					extractPartSeq: seqSeqType[i][int(refIdenStart[j]):int(refIdenEnd[j])],
+				})
 			}
-	alignmentOpen, err := os.Open("aligned.tsv")
+		}
+	}
+
+	file, err := os.Create("sequences-annotation.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	for i := range extractPartSeq {
+		_, err := file.WriteString(
+			">" + extractPartSeq[i].extractPartID + "\n" + extractPartSeq[i].extractPartSeq + "\n",
+		)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+}
+
+func upstreamFunc(cmd *cobra.Command, args []string) {
+	refID := []string{}
+	alignID := []string{}
+	refIdenStart := []float64{}
+	refIdenEnd := []float64{}
+	alignIdenStart := []float64{}
+	alignIdenEnd := []float64{}
+	fOpen, err := os.Open(alignmentfile)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fRead := bufio.NewScanner(fOpen)
+
+	for fRead.Scan() {
+		line := fRead.Text()
+		refID = append(refID, strings.Split(string(line), "\t")[0])
+		alignID = append(alignID, strings.Split(string(line), "\t")[1])
+		start1, _ := strconv.ParseFloat(strings.Split(string(line), "\t")[6], 32)
+		end1, _ := strconv.ParseFloat(strings.Split(string(line), "\t")[7], 32)
+		start2, _ := strconv.ParseFloat(strings.Split(string(line), "\t")[8], 32)
+		end2, _ := strconv.ParseFloat(strings.Split(string(line), "\t")[9], 32)
+		refIdenStart = append(refIdenStart, start1)
+		refIdenEnd = append(refIdenEnd, end1)
+		alignIdenStart = append(alignIdenStart, start2)
+		alignIdenEnd = append(alignIdenEnd, end2)
+	}
+
+	seqIDType, seqSeqType := readRef()
+
+	type extractStreamSeq struct {
+		extractStreamPartID  string
+		extractStreamPartSeq string
+	}
+
+	extractupstreamPartSeq := []extractStreamSeq{}
+
+	for i := range seqIDType {
+		for j := range refID {
+			if seqIDType[i] == refID[j] {
+				extractupstreamPartSeq = append(extractupstreamPartSeq, extractStreamSeq{
+					extractStreamPartID:  seqIDType[i],
+					extractStreamPartSeq: seqSeqType[i][int(int(refIdenStart[j])-upstreamStart):int(downstreamEnd+int(refIdenEnd[j]))],
+				})
+			}
+		}
+
+		file, err := os.Create("sequences-annotation-upstream-downstream.txt")
+		if err != nil {
+			log.Fatal(err)
+		}
+		for i := range extractupstreamPartSeq {
+			_, err := file.WriteString(
+				">" + extractupstreamPartSeq[i].extractStreamPartID + "\n" + extractupstreamPartSeq[i].extractStreamPartSeq + "\n",
+			)
 			if err != nil {
 				log.Fatal(err)
 			}
-	alignmentRead, err := bufio.NewScanner(alignmentOpen)
-	holdAlignment := []hspStruct{}
-	for alignmentRead.Scan() {
-				line := alignmentRead.Text()
-				holdAlignment := append(holadalignment, hspStruct{
-					refseq : strings.Split(line, "\t")[0]
-					aligned : strings.Split(line, "\t")[1]
-					alignedMatch : strings.Split(line, "\t")[2]
-					start : strings.Split(line, "\t")[6]
-					end : strings.Split(line, "\t")[7]
-					evalue: strings.Split(line, "\t")[11]
-				})
-			}
-
-  type nucleotideIDStruct struct {
-				id string
-			}
-
-	type nucleotideSeqstruct struct {
-				seq string
-			}
-
-	type alignedFetch struct {
-				seqID string
-				seqSeq string
-			}
-
-  hspSeq := []alignedFetch{}
-	for i := range holdAlignment {
-		for j := range seqIDWrite {
-			if seqIDwrite[i] == holdAlignment[i].refseq {
-					hspSeq = append(hspSeq, alignedFetch{
-						seqID : seqIDwrite[i]
-						seqSeq : SeqSeqWrite[i][int(holdAlignment[i].start):int(holdAlignment[i].end)]
-					})
-				}
-			}
-				}
-
-	writeHsp, err := os.Create("writeHSP", os.O_CREATE|os.O_WRONLY, 0644)
-				if err != nil {
-					log.Fatal(err)
-				}
-	defer writeHsp.Close()
-				for i:= range hspSeq {
-					write, err := writeHsp.write(>hspSeq[i].seqID\nhspseqSeq[i]\n)
-				}
+		}
+	}
 }
 
+func alignAnalyzeFunc(cmd *cobra.Command, args []string) {
+	readOpen, err := os.Open(referencefasta)
+	if err != nil {
+		log.Fatal(err)
 	}
+
+	readbuffer := bufio.NewScanner(readOpen)
+	header := []string{}
+	sequences := []string{}
+	length := []float64{}
+
+	for readbuffer.Scan() {
+		line := readbuffer.Text()
+		if string(line[0]) == "A" || string(line[0]) == "T" || string(line[0]) == "G" ||
+			string(line[0]) == "C" {
+			sequences = append(sequences, line)
+		}
+		if string(line[0]) == ">" {
+			header = append(header, strings.ReplaceAll(string(line), ">", ""))
+		}
+	}
+	for i := range sequences {
+		length = append(length, float64(len(sequences[i])))
+	}
+
+	refID := []string{}
+	alignID := []string{}
+	refIdenStart := []float64{}
+	refIdenEnd := []float64{}
+	alignIdenStart := []float64{}
+	alignIdenEnd := []float64{}
+	fOpen, err := os.Open(alignmentfile)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fRead := bufio.NewScanner(fOpen)
+
+	for fRead.Scan() {
+		line := fRead.Text()
+		refID = append(refID, strings.Split(string(line), "\t")[0])
+		alignID = append(alignID, strings.Split(string(line), "\t")[1])
+		start1, _ := strconv.ParseFloat(strings.Split(string(line), "\t")[6], 32)
+		end1, _ := strconv.ParseFloat(strings.Split(string(line), "\t")[7], 32)
+		start2, _ := strconv.ParseFloat(strings.Split(string(line), "\t")[8], 32)
+		end2, _ := strconv.ParseFloat(strings.Split(string(line), "\t")[9], 32)
+		refIdenStart = append(refIdenStart, start1)
+		refIdenEnd = append(refIdenEnd, end1)
+		alignIdenStart = append(alignIdenStart, start2)
+		alignIdenEnd = append(alignIdenEnd, end2)
+	}
+	id, _, length := pacbio()
+
+	type cov struct {
+		id  string
+		cov float64
+	}
+
+	coverageSeq := []cov{}
+	for i := range id {
+		for j := range refID {
+			if id[i] == refID[j] {
+				coverageSeq = append(coverageSeq, cov{
+					id:  refID[j],
+					cov: (refIdenEnd[j] - refIdenStart[j]) / length[i] * 100,
+				})
+			}
+		}
+	}
+
+	for i := range coverageSeq {
+		fmt.Println(coverageSeq[i].id, coverageSeq[i].cov)
+	}
+
+	file, err := os.Create("coveragestimation.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+	for i := range coverageSeq {
+		storeI := strconv.FormatFloat(coverageSeq[i].cov, 'f', -1, 64)
+		_, err := file.WriteString(coverageSeq[i].id + "\t" + storeI + "\n")
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	seqIDType, seqSeqType := readRef()
+
+	type extractSeq struct {
+		extractPartID  string
+		extractPartSeq string
+	}
+
+	extractPartSeq := []extractSeq{}
+
+	for i := range seqIDType {
+		for j := range refID {
+			if seqIDType[i] == refID[j] {
+				extractPartSeq = append(extractPartSeq, extractSeq{
+					extractPartID:  seqIDType[i],
+					extractPartSeq: seqSeqType[i][int(refIdenStart[j]):int(refIdenEnd[j])],
+				})
+			}
+		}
+	}
+
+	file1, err := os.Create("sequences-annotation.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	for i := range extractPartSeq {
+		_, err := file1.WriteString(
+			">" + extractPartSeq[i].extractPartID + "\n" + extractPartSeq[i].extractPartSeq + "\n",
+		)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	type extractStreamSeq struct {
+		extractStreamPartID  string
+		extractStreamPartSeq string
+	}
+
+	extractupstreamPartSeq := []extractStreamSeq{}
+
+	for i := range seqIDType {
+		for j := range refID {
+			if seqIDType[i] == refID[j] {
+				extractupstreamPartSeq = append(extractupstreamPartSeq, extractStreamSeq{
+					extractStreamPartID:  seqIDType[i],
+					extractStreamPartSeq: seqSeqType[i][int(int(refIdenStart[j])-upstreamStart):int(downstreamEnd+int(refIdenEnd[j]))],
+				})
+			}
+		}
+
+		file, err := os.Create("sequences-annotation-upstream-downstream.txt")
+		if err != nil {
+			log.Fatal(err)
+		}
+		for i := range extractupstreamPartSeq {
+			_, err := file.WriteString(
+				">" + extractupstreamPartSeq[i].extractStreamPartID + "\n" + extractupstreamPartSeq[i].extractStreamPartSeq + "\n",
+			)
+			if err != nil {
+				log.Fatal(err)
+			}
+		}
+	}
+}
+
+func readRef() ([]string, []string) {
+	extractID := []string{}
+	seqID := []string{}
+
+	readF, err := os.Open(referencefasta)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	openF := bufio.NewScanner(readF)
+
+	for openF.Scan() {
+		line := openF.Text()
+		if string(line[0]) == "A" || string(line[0]) == "T" || string(line[0]) == "G" ||
+			string(line[0]) == "C" {
+			seqID = append(seqID, line)
+		}
+		if string(line[0]) == ">" {
+			extractID = append(extractID, strings.ReplaceAll(string(line), ">", ""))
+		}
+	}
+	return extractID, seqID
 }
